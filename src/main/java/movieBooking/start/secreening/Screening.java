18e44303a1672ea7a951898b69477df79798c5a1
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-03 00:08:15 +0900

    Screening (상영) 엔티티 생성

---
+++ src/main/java/movieBooking/start/secreening/Screening.java
@@ -0,0 +1,43 @@
+package movieBooking.start.secreening;
+
+import lombok.Getter;
+import lombok.Setter;
+import movieBooking.start.movie.Movie;
+import movieBooking.start.theater.Theater;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+// 고객이 예매를 위해 상영을 조회할 경우, 전체좌석과 예매 가능한 좌석을 함께 보여줘야 한다.
+public class Screening {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "SCREENING_ID")
+    private Long id;
+
+    // 영화 이름
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MOVIE_ID")
+    private Movie movie;
+
+    // 시작시간
+    private LocalTime startTime;
+
+    // 종료시간
+    // 종료시간 = 시작시간.plusMinutes(movie.getRunningTime())
+    private LocalTime endTime;
+
+    // 상영관 이름
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "THEATER_ID")
+    private Theater theater;
+
+    /* 로직 */
+    public LocalTime calEndTime() {
+        return startTime.plusMinutes(movie.getRunningTime());
+    }
+}
