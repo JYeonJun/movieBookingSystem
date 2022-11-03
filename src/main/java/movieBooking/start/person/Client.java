f81b0f4b9ef9d38cca213cda2c2974cc191899fb
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-03 21:16:10 +0900

    Client 엔티티에 필드 추가

--- src/main/java/movieBooking/start/person/Client.java
+++ src/main/java/movieBooking/start/person/Client.java
@@ -2,15 +2,24 @@ package movieBooking.start.person;
 
 import lombok.Getter;
 import lombok.Setter;
+import movieBooking.start.ticketing.Ticketing;
 
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @DiscriminatorValue("C")
-public class Client extends Person{
+public class Client extends Person {
 
+    // 마일리지
+    private Integer mileage;
 
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ticketing> ticketingList = new ArrayList<Ticketing>();
 }
