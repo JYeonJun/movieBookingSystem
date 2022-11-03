9a8df3f2492cd0c6059625a743278bec559a61d8
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-03 22:22:45 +0900

    Seat 수정

--- src/main/java/movieBooking/start/theater/Seat.java
+++ src/main/java/movieBooking/start/theater/Seat.java
@@ -16,15 +16,24 @@ public class Seat {
     private Long id;
 
     // 행
-    private Integer row;
+    private Integer lineNumber;
 
     // 열
-    private Integer column;
+    private Integer columnNumber;
 
     // 좌석 상태 [가용 (AVAILABLE), 불가용 (UNAVAILABLE)]
     private SeatState seatState;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "MOVIE_ID")
+    @JoinColumn(name = "THEATER_ID")
     private Theater theater;
+
+    /* 연관관계 편의 메서드*/
+    public void setTheater(Theater theater) {
+        if (this.theater != null) {
+            this.theater.getSeats().remove(this);
+        }
+        this.theater = theater;
+        theater.getSeats().add(this);
+    }
 }
