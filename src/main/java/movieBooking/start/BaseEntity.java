a1dd8a5f79d315a60b9c44b6c56e299e145175ec
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-03 21:14:40 +0900

    BaseEntity에 Getter, Setter 추가

--- src/main/java/movieBooking/start/BaseEntity.java
+++ src/main/java/movieBooking/start/BaseEntity.java
@@ -1,10 +1,14 @@
 package movieBooking.start;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter @Setter
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     private LocalDateTime createdDate;
 
