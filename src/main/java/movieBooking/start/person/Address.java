2ca78068887060527ee71b67f3c6c79a58ceb3c7
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-03 21:15:56 +0900

    Address 클래스에 ToString 추가

--- src/main/java/movieBooking/start/person/Address.java
+++ src/main/java/movieBooking/start/person/Address.java
@@ -1,9 +1,6 @@
 package movieBooking.start.person;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Embeddable;
 
@@ -11,6 +8,7 @@ import javax.persistence.Embeddable;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Address {
 
     private String city;
